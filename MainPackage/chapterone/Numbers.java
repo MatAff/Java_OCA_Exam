03170bc8af3e52cfdb738afc156727c4a80c2afc
MatAff <mathijsaffourtit@gmail.com>
2017-03-10 17:08:33 -0700

    Organize code

---
+++ MainPackage/chapterone/Numbers.java
@@ -0,0 +1,22 @@
+package MainPackage.chapterone;
+
+public class Numbers
+{
+
+    int myInt;
+    long myLong;
+    float myFloat;
+    
+    public static void main(String []args) {
+        
+        Numbers n = new Numbers();
+        
+        n.myLong = 55;
+        n.myFloat = 55.0F;
+        
+        n.myInt = 0b10;
+
+        
+    }
+
+}
